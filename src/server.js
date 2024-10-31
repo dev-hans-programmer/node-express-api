98e7f726a11825b0c79a1f5ea79fb020a4b28ab4
Hasan Ali <hans1998ali@gmail.com>
2024-10-31 16:59:14 +0530

    Unhandled and uncaught error

--- src/server.js
+++ src/server.js
@@ -8,14 +8,38 @@ const connectDB = () => mongoose.connect(Config.DB_URI);
 
 const port = Config.PORT || 9000;
 
+let server;
+
+function closeServer(err, isUncaught = false) {
+  logger.info(err.name);
+  logger.error(err.message);
+  logger.info(
+    `${isUncaught ? 'Uncaught Exception' : 'Unhandled rejection'}, shutting down gracefully`
+  );
+  if (server) server.close(() => process.exit(1));
+  else process.exit(1);
+}
+process.on('uncaughtException', (err) => {
+  closeServer(err, true);
+});
+
 const startServer = async () => {
   try {
     await connectDB();
     logger.info(`DB connected successfully`);
-    app.listen(port, () => logger.info(`App running on port ${port}...`));
+
+    server = app.listen(port, () =>
+      logger.info(`App running on port ${port}...`)
+    );
   } catch (err) {
     logger.error(err.message);
+    process.exit(1);
   }
 };
+
+// unhandled promise rejection error throughtout our app
+process.on('unhandledRejection', (err) => {
+  closeServer(err);
+});
 
 startServer();
