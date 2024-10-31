e75ad7b50aa8ae039e51e38ffb1c6a2c9efa2ea2
Hasan Ali <hans1998ali@gmail.com>
2024-10-31 15:08:50 +0530

    Separated dev and prod error

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,11 +1,36 @@
+const Config = require('../config');
 const { logger } = require('../config/logger');
 
-exports.globalErrorHandler = (err, req, res, next) => {
-  logger.error(err.stack);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendDevError = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    stack: err.stack,
   });
+};
+
+const sendProdError = (err, res) => {
+  // operational: trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // unknown error, programming error: don't leak details
+  logger.error(err);
+  res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong',
+  });
+};
+
+exports.globalErrorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (Config.NODE_ENV === 'production') {
+    return sendProdError(err, res);
+  }
+  return sendDevError(err, res);
 };
